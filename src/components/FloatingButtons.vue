797ba8b9c11ca367d6eacba72e24e6109e7d3017
Lennin5 <lenninlemus0@gmail.com>
2023-09-20 14:53:56 -0600

    added horizontal fixed buttons

--- src/components/FloatingButtons.vue
+++ src/components/FloatingButtons.vue
@@ -1,72 +1,67 @@
 <template>
-    <div class="floating-buttons">
+    <div class="floating-buttons" 
+    style="border: 1px solid red">
+
+    <!-- <div class="containerr" style="border: 1px solid blue"> -->
       <!-- Botón A -->
       <v-btn
+        :key="key"
+        v-for="(n, key) in 20"
         fab
-        large
+        small
         color="primary"
         class="ma-2"
-        @click="handleButtonClick('A')"
+        @click="handleButtonClick(n)"
       >
-        A
-      </v-btn>
-  
-      <!-- Botón B -->
-      <v-btn
-        fab
-        large
-        color="primary"
-        class="ma-2"
-        @click="handleButtonClick('B')"
-      >
-        B
-      </v-btn>
-  
-      <!-- Botón C -->
-      <v-btn
-        fab
-        large
-        color="primary"
-        class="ma-2"
-        @click="handleButtonClick('C')"
-      >
-        C
+        {{ n }}
       </v-btn>
 
-        <!-- Botón D -->
-        <v-btn
-        fab
-        large
-        color="primary"
-        class="ma-2"
-        @click="handleButtonClick('D')"
-        >
-        D
-        </v-btn>
     </div>
+
+
+    <!-- </div> -->
   </template>
   
   <script>
+  import pokeApi from '../plugins/axios';
   export default {
+    created() {
+      this.getPokemonTypes()
+    },
     methods: {
       handleButtonClick(buttonText) {
         // Aquí puedes manejar la lógica cuando se hace clic en uno de los botones.
         // Por ejemplo, puedes mostrar un mensaje o realizar una acción específica.
         console.log(`Botón ${buttonText} clickeado.`);
       },
+      async getPokemonTypes(){
+        try {
+          const response = await pokeApi.get('/type');
+          console.log(response, 'pokemonsTypes');          
+        } catch (error) {
+          console.log(error, 'error');
+        }
+      }
     },
   };
   </script>
   
   <style>
-  .floating-buttons {
+  .floating-buttons {    
     position: fixed;
-    top: 250px; /* Ajusta la posición vertical según tus necesidades */
-    right: 16px; /* Ajusta la posición horizontal según tus necesidades */
+    background-color: black;
+    width: 100%;
+    bottom: 0px;
     display: flex;
-    flex-direction: column;
+    justify-content: center;
+    flex-direction: row;
     align-items: center;
-    z-index: 999; /* Asegura que los botones estén sobre otros elementos */
+    z-index: 999;
+  }
+  .containerr{
+    height: 100px;
+    display: flex;
+    flex-direction: row;
   }
   </style>
   
