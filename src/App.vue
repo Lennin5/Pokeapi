b9b2ac7c5ce8275aed8e6cef31258fbda643bccb
Lennin5 <lenninlemus0@gmail.com>
2023-09-22 15:17:39 -0600

    first changes of new update sidebar menu

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-navigation-drawer
       app
-      color="orange"
-      dark
+      v-model="drawer"
+      :mini-variant.sync="mini"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="2"
-          :src="LogoPokemons"
-          style="width: 200px;"
-        />
-      </div>
-      <v-btn href="/" text>
-        <span class="mr-2">Home</span>
-        <v-icon>mdi-home</v-icon>
-      </v-btn>   
-      <v-btn href="/type" text>
-        <span class="mr-2">Types</span>
-        <v-icon>mdi-bookmark-box-multiple</v-icon>
-      </v-btn>      
+      <!-- Logo o título -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            <img :src="LogoPokemons" alt="Logo" height="32" />
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-      <v-spacer></v-spacer>
+      <!-- Botón para expandir/contraer el menú -->
+      <v-list-item @click="toggleMenu">
+        <v-list-item-icon>
+          <v-icon>{{ mini ? 'mdi-menu-open' : 'mdi-menu' }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ mini ? 'Expandir' : 'Contraer' }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>      
 
-      <v-btn
-        href="https://pokeapi.co/api/v2/pokemon/"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Pokeapi</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <!-- Contenido del menú (iconos) -->
+      <v-list dense>
+        <v-list-item
+          v-for="(item, index) in menuItems"
+          :key="index"
+          @click="menuItemClicked(item)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+
+    </v-navigation-drawer>
+
+    <!-- Contenido principal -->
     <v-main style="background-color: #f2f2f2;">
+      <!-- Contenido principal de la aplicación -->
       <router-view></router-view>
     </v-main>
   </v-app>
@@ -50,7 +60,28 @@ export default {
   name: 'App',
 
   data: () => ({
-    LogoPokemons: LogoPokemons
+    LogoPokemons: LogoPokemons,
+    drawer: true, // Controla el estado de la barra lateral (colapsada o expandida)
+    mini: false, // Controla el estado mini del menú (solo iconos)
+    menuItems: [ // Define los elementos del menú
+      { text: 'Opción 1', icon: 'mdi-home' },
+      { text: 'Opción 3', icon: 'mdi-account' },
+    ],
   }),
+
+  methods: {
+    menuItemClicked(item) {
+      // Agrega lógica para manejar la selección de elementos del menú aquí
+      console.log('Elemento del menú seleccionado:', item.text);
+    },
+
+    toggleMenu() {
+      this.mini = !this.mini; // Alterna entre el estado mini y expandido del menú
+    },
+  },
 };
 </script>
+
+<style>
+/* Agrega estilos personalizados según tus necesidades */
+</style>
