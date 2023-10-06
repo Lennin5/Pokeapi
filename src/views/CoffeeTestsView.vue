b380b312f9fcebc0e0421854468c43833753bfa0
Lennin5 <lenninlemus0@gmail.com>
2023-10-06 09:39:40 -0600

    feat(carrusel principal) agregado de libreria animate.css y efecto fade in

--- src/views/CoffeeTestsView.vue
+++ src/views/CoffeeTestsView.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
+    <!-- <h1 class="animate__animated animate__bounce">An animated element</h1> -->
     <v-carousel
       cycle
       vertical
-        :show-arrows="false"
-        delimiter-icon="mdi-minus"
-        hide-delimiter-background      
+      :show-arrows="false"
+      delimiter-icon="mdi-minus"
+      hide-delimiter-background
+      :interval="5000"
     >
       <v-carousel-item
         v-for="(item, index) in items"
         :key="index"
       >
-        <img :src="item.image" alt="Imagen" />
+        <img :src="item.image" alt="Imagen" key="item.image" class="animate__animated animate__fadeIn">          
       </v-carousel-item>
     </v-carousel>
   </div>
@@ -38,5 +40,5 @@ export default {
 </script>
 
 <style scoped>
-/* Estilos personalizados si es necesario */
+
 </style>
