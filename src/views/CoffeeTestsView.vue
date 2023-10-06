c71f00a8edf176f82100d6f5e49338d2d9d47e6f
Lennin5 <lenninlemus0@gmail.com>
2023-10-06 10:27:10 -0600

    style(carrusel principal) agregado de circulo sin animación

--- src/views/CoffeeTestsView.vue
+++ src/views/CoffeeTestsView.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
+  <div style="border: 1px solid red">
     <!-- <h1 class="animate__animated animate__bounce">An animated element</h1> -->
+    <div class="contenedor">
+        <div class="circulo">
+            <!-- Contenido dentro del círculo si es necesario -->
+        </div>
+    </div>    
     <v-carousel
       cycle
       vertical
@@ -9,10 +14,11 @@
       hide-delimiter-background
       :interval="5000"
     >
+  
       <v-carousel-item
         v-for="(item, index) in items"
         :key="index"
-      >
+      >        
         <img :src="item.image" alt="Imagen" key="item.image" class="w-100 animate__animated animate__fadeIn" >          
       </v-carousel-item>
     </v-carousel>
@@ -47,5 +53,25 @@ export default {
 </script>
 
 <style scoped>
+/* Estilos para el contenedor exterior */
+.contenedor {
+  z-index: 1;
+    position: absolute;
+    width: 1200px; /* Ancho del contenedor */
+    height: 1200px; /* Altura del contenedor */
+    background-color: #fff; /* Color de fondo rojo */
+    border-radius: 50%; /* Hace que el contenedor exterior sea un círculo */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: -700px;
+}
 
+/* Estilos para el círculo interior */
+.circulo {
+    width: 50%; /* Ancho del círculo interior en relación al contenedor */
+    height: 50%; /* Altura del círculo interior en relación al contenedor */
+    background-color: rgb(145, 145, 145); /* Color de fondo azul */
+    border-radius: 50%; /* Hace que el círculo interior sea un círculo */
+}
 </style>
